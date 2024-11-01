2fc9f798474217f817bec1c172ebcce43c232c94
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-11-01 15:10:34 +0400

    Change settings icon background color, and remove horizontal padding of header to be same width as main content

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,10 @@ import { VscSettingsGear } from 'react-icons/vsc';
 
 function Header() {
     return (
-        <div class="p-4 mb-4">
+        <div class="py-4 mb-4">
             <div className="d-flex flex-row">
                 <h1 style={{marginBottom: 0}}>Probolab</h1>
-                <Button className="ms-auto fs-2 d-flex" color="light" style={{padding: 0}}>
+                <Button className="ms-auto fs-2 d-flex" color="dark" style={{padding: 0}}>
                     <VscSettingsGear className="my-auto mx-2" />
                 </Button>
             </div>
