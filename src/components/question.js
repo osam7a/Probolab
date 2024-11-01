297605056072164bee563f3b7ed01e6aef22524a
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-11-01 15:12:27 +0400

    Multiple changes to Question component include: - Set the width of problem_subject to be 'max-content' - Input text for answers, with a new parameter "hide_input" to hide the input - Steps section, not-complete

--- src/components/question.js
+++ src/components/question.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
 
 function Question(props) {
     var question_data = props.question_data;
@@ -7,23 +6,33 @@ function Question(props) {
     var problem_description = question_data[1];
     var problem_answer = question_data[2];
     var problem_steps = question_data[3];
-    var problem_subject = question_data[4]
+    var problem_subject = question_data[4];
+    var hide_input = question_data[5];
 
     return (
         <div className="fieldset flex-grow-1 d-flex flex-column">
-            <span>{problem_subject}</span>
-            <div className="main flex-grow-1 d-flex flex-column">
+            <span style={{width: "max-content"}}>{problem_subject}</span>
+            <div className="main flex-grow-1 d-flex flex-column gap-3">
                 <div className="row gap-3 flex-grow-1 flex-md-grow-0">
-                    <div className="col-md">
+                    <div className="d-flex flex-column col-md p-0">
                         <h1 id="question_text">{problem_text}</h1>
                         <h3 className="text-muted">{problem_description}</h3>
+                        {hide_input ? null :
+                        <div className="mt-auto" id="prediction-container">
+                            <div className="input-group">
+                                <span className="input-group-text" id="prediction-label">Test</span>
+                                <input type="text" className="form-control" placeholder="Enter your prediction here" aria-label="Username" aria-describedby="basic-addon1" id="prediction" />
+                            </div>
+                        </div>
+                        }
                     </div>
-                    <div className="col-md order-md-last order-first">
+                    <div className="col-md order-md-last order-first p-0">
                         <canvas style={{border: "1px solid red"}} className="w-100 h-100"></canvas>
                     </div>
                 </div>
-                <div className="row flex-md-grow-1">
-                    <Button style={{backgroundColor: "#759aab"}}>test</Button>
+                <div className="row flex-md-grow-1 d-flex h-100 border border-dark rounded p-3 flex-column">
+                    <div id="steps" style={{flexGrow: 1, overflowY: 'auto', height: '200px'}}>
+                    </div>
                 </div>
             </div>
         </div>
